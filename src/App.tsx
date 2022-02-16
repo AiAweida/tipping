6dc88c389ad4c6f5a79d5876414ef5544b1855bc
Adib Aweida <AAweida@asaltech.com>
2022-02-16 15:57:55 +0200

    first branch commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,17 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">welcome</div>;
+  return (
+    <div className="App">
+      <header className="App__Header">
+        SPLI<p>TTER</p>
+      </header>
+      <div className="App__Container">
+        <div className="App__Input"></div>
+        <div className="App__Output"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
